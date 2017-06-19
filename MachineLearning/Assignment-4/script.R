d1d71900d0a9d34088161f6bad13d69e58ead336
Eduardo Santana <ezambomsantana@gmail.com>
2017-06-18 23:50:34 -0300

    Update script.R

--- MachineLearning/Assignment-4/script.R
+++ MachineLearning/Assignment-4/script.R
@@ -1,5 +1,7 @@
 library(caret)
 library(randomForest)
+library(rpart) # Regressive Partitioning and Regression trees
+library(e1071)
 
 set.seed(1234)
 
@@ -10,15 +12,23 @@ trainingData <- read.csv(url(trainUrl), na.strings=c("NA","#DIV/0!",""))
 testingData <- read.csv(url(testUrl), na.strings=c("NA","#DIV/0!",""))
 
 
+trainingData <- trainingData[,colSums(is.na(trainingData)) == 0]
+testingData <- testingData[,colSums(is.na(testingData)) == 0]
+trainingData <- trainingData[,-c(1:7)]
+testingData <- testingData[,-c(1:7)]
+
 train <- createDataPartition(y=trainingData$classe,p=.70,list=F)
 training <- trainingData[train,]
 testing <- trainingData[-train,]
 
-Cl <- grep("name|timestamp|window|X", colnames(training), value=F) 
-trainingCl <- training[,-Cl]
 
-#select variables with high (over 95%) missing data --> exclude them from the analysis
-trainingCl[trainingCl==""] <- NA
-NArate <- apply(trainingCl, 2, function(x) sum(is.na(x)))/nrow(trainingCl)
-trainingCl <- trainingCl[!(NArate>0.95)]
-summary(trainingCl)
+model1 <- rpart(classe ~ ., data=training, method="class")
+prediction1 <- predict(model1, testing, type = "class")
+confusionMatrix(prediction1, testing$classe)
+
+
+
+
+model2 <- randomForest(classe ~. , data=training, method="class")
+prediction2 <- predict(model2, testing, type = "class")
+confusionMatrix(prediction2, testing$classe)
