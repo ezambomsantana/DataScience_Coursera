a1baccc21a340fb53430eacc9d79271e3ce1f9b4
Eduardo Santana <ezambomsantana@gmail.com>
2017-10-27 17:53:51 -0200

    Update server.R

--- capstone_project/app/server.R
+++ capstone_project/app/server.R
@@ -78,17 +78,4 @@ shinyServer(function(input, output) {
     input$entry
   })
   
-  
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
 })
